0cdac31d1138edeec91022d02ef1bceed9a6d4a7
borislav nikolov <jack@sofialondonmoskva.com>
2020-04-15 10:03:53 +0200

    use ssh germinal to get width

--- pkg/util/size.go
+++ pkg/util/size.go
@@ -3,32 +3,12 @@ package util
 import (
 	"os"
 	"strconv"
-	"syscall"
-	"unsafe"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-type winsize struct {
-	Row    uint16
-	Col    uint16
-	Xpixel uint16
-	Ypixel uint16
-}
-
-func getConsoleWidth() uint {
-	ws := &winsize{}
-	retCode, _, _ := syscall.Syscall(syscall.SYS_IOCTL,
-		uintptr(syscall.Stdin),
-		uintptr(syscall.TIOCGWINSZ),
-		uintptr(unsafe.Pointer(ws)))
-
-	if int(retCode) == -1 {
-		return 80
-	}
-	return uint(ws.Col)
-}
-
 func GetWidth() uint {
-	w := getConsoleWidth()
+
 	manwidth := os.Getenv("MANWIDTH")
 	if manwidth != "" {
 		v, err := strconv.ParseInt(manwidth, 10, 64)
@@ -36,5 +16,6 @@ func GetWidth() uint {
 			return uint(v)
 		}
 	}
-	return w
+	width, _, _ := terminal.GetSize(0)
+	return uint(width)
 }
